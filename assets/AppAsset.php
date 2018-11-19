d8f43fe5af07e0e7946858d4555728b02dd2ff01
marcosmfilho <marcosmfilho@gmail.com>
2018-11-19 13:10:02 -0300

    remove jquery cdn

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -23,7 +23,7 @@ class AppAsset extends AssetBundle
         'css/site.css',
     ];
     public $js = [
-        //'js/jquery.js',
+        //'js/jquery-3.3.1.min.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
--- views/notices/index.php
+++ views/notices/index.php
@@ -10,11 +10,7 @@ $this->title = 'Notícias';
 //$this->params['breadcrumbs'][] = $this->title;
 ?>
 
-<script
-  src="https://code.jquery.com/jquery-3.3.1.min.js"
-  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-  crossorigin="anonymous">
-</script>
+<script src="/js/jquery-3.3.1.min.js"></script>
 
 <div class="notices-index">
 
